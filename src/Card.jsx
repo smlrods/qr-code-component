13a3bf399aad6176a0426ae4a3ec1fcd43f60806
sml <smlrodrigues.dev@gmail.com>
2023-04-06 18:04:29 -0300

    Style the page

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,6 +4,11 @@ import './Card.css'
 function Card() {
   return (
     <div id='card' >
+      <div className='qrcode'></div>
+      <div className='main'>
+        <h2>Improve your front-end skills by building projects</h2>
+        <div className='description'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+      </div>
     </div>
   )
 }
